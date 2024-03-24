65e80966d9beb26486f6ce5ce6c68b1878dfd486
Toni Tiilikainen <ototi009@edu.xamk.fi>
2024-03-24 18:15:40 +0200

    ip thing change thing

--- Assets/Scripts/Networking/RpcTest.cs
+++ Assets/Scripts/Networking/RpcTest.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class RpcTest : NetworkBehaviour
 {
@@ -7,9 +9,31 @@ public class RpcTest : NetworkBehaviour
     {
         if (!IsServer && IsOwner) //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
         {
-            TestServerRpc(0, NetworkObjectId);
+            // Lightsail server endpoint
+            string serverURL = "51.20.125.76";
+            StartCoroutine(SendRequestCoroutine(serverURL));
+            
+            // Original code:
+            // TestServerRpc(0, NetworkObjectId);
         }
     }
+    
+    IEnumerator SendRequestCoroutine(string serverURL)
+    {
+        // Sending HTTP GET request to the server
+        UnityWebRequest request = UnityWebRequest.Get(serverURL);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Failed to send request: " + request.error);
+        }
+        else
+        {
+            Debug.Log("Server response: " + request.downloadHandler.text);
+        }
+    }
+    
 
     [Rpc(SendTo.ClientsAndHost)]
     void TestClientRpc(int value, ulong sourceNetworkObjectId)
